8ed0144bfa426e845d51feb96fea75e0df56131a
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-02-03 10:49:08 -0800

    adds cookie-parser middleware to handle working with auth cookie, mounts cors options in middleware

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 ////////////////////////
-// Setup - Import deps and create app object
+// Setup - Import deps and create app
 ////////////////////////
-
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import dotenv from 'dotenv'
-
+import cookieParser from 'cookie-parser'
+import corsOptions from './utils/cors.js'
 dotenv.config()
 const app = express()
-
 //////////////////////
 // Declare Middleware
 //////////////////////
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(morgan("dev"))
 app.use(express.json())
+app.use(cookieParser())
 
 ///////////////////////
 // Declare Routes and Routers 
